ff92ec7b55fd35b04b27237aaf198efffef54f8d
Larry <tradesouthwest@outlook.com>
2017-06-05 23:16:39 -0700

    added to deactivation
    also added inline comments

--- clientflow.php
+++ clientflow.php
@@ -15,7 +15,7 @@ if ( ! function_exists( 'add_action' ) ) {
 	die( 'Nothing to see here...' );
 }
 /* Important constants */
-define( 'CLFL_VERSION', '0.1.0' );
+//define( 'CLFL_VERSION', '0.1.0' );
 define( 'CLFL_FORMS_URL', plugin_dir_url( __FILE__ ) );
 
 //activate/deactivate hooks
@@ -28,6 +28,9 @@ function clientflow_plugin_deactivation() {
 
     clfl_delete_post_type();
     add_action('init','clfl_delete_post_type');
+    //remove_filter( 'wp_mail_content_type', 'clientflow_set_html_mail_content_type' );
+    //remove_filter( 'wp_mail_from', 'clientflow_wp_mail_from' );
+    remove_action('init','clfl_delete_post_type');
     //flush_rewrite_rules();
         return false;
 }
@@ -56,20 +59,15 @@ add_action('plugins_loaded', 'clientflow_loadtranslations');
  */
 function clientflow_scripts() {
 
-    // Register Scripts
+   // Register Scripts - note: v 0.1.0 not using ajax but script can be used for validate
     wp_register_script( 'clientflow', plugins_url(
                         'js/clientflow.js', __FILE__ ), array( 'jquery' ), true );
     // Register Styles
     wp_register_style( 'clientflow-style', CLFL_FORMS_URL . 'css/clientflow-style.css' );
 
     wp_enqueue_style( 'clientflow-style' );
-    wp_enqueue_script( 'clientflow' );
-   /* wp_enqueue_script( 'clientflowajax', get_template_directory_uri().'/js/clientflowajax.js',
-                        array('jquery') );
-    //ajax form submit
-    wp_localize_script( 'clientflowajax.js', 'clflFormJSObject', array(
-                        'ajaxUrl' => admin_url( 'admin-ajax.php' ) //url to process ajax request to WP
-                        ) ); */
+    //wp_enqueue_script( 'clientflow' );
+	
 }
 add_action( 'wp_enqueue_scripts', 'clientflow_scripts' );
 
@@ -77,7 +75,6 @@ register_activation_hook( __FILE__, 'clientflow_plugin_activation');
 register_deactivation_hook( __FILE__, 'clientflow_plugin_deactivation');
 
 //include admin and public views
-//require ( plugin_dir_path( __FILE__ ) . 'inc/clientflow-posttype-client.php' );
 require ( plugin_dir_path( __FILE__ ) . 'inc/clientflow-adminpage.php' );
 require ( plugin_dir_path( __FILE__ ) . 'inc/clientflow-formpage.php' );
 ?>
